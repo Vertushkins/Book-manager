85357c05a2e33571a8cfaf408595e4ed9df80678
georgy <solodyankin.gm@gamil.com>
2023-12-01 15:49:25 +0300

    Book manager v2 1. Library shows the list of all books by default. 2. Filter buttons replaced by radio buttons. 3. Forms divided by lines. 4. Added a warning about empty fields in 'add book' form. 5. Added icons near the name of forms. 6. Added book searching in 'library'. 7. Added comments in code.

--- index.php
+++ index.php
@@ -3,50 +3,28 @@
 	
 	$pdo = new PDO($attr, $user, $pass, $opts);
 	
-	echo <<< _END
-	<form action="" method="post">
-	<h2><b>Add book</b></h2>
-	<pre>
-	<big>Author</big> <input type="text" name="author" placeholder="..."><br>
-	<big>Title</big>  <input type="text" name="title" placeholder="..."><br>
-	<big>Pages</big>  <input type="text" name="pages" placeholder="..."><br>
-	<input type="submit" name="add" value="Click to add"><br>
-	</pre>
-	</form>
-	_END;
-	
-	echo <<< _END
-	<form action="" method="post">
-	<h2><b>Edit book</b></h2>
-	<pre>
-	<big>Number  </big>  <input type="text" name="id" placeholder="..."><br>
-	<big>Bookmark</big>  <input type="text" name="bookmark" placeholder="..."><br>
-	<big>Last view</big> <input type="text" name="last_view" placeholder="dd.mm.yyyy"><br>
-	<input type="submit" name="edit" value="Click to edit"><br>
-	</pre>
-	</form>
-	_END;
-	
-	echo <<< _END
-	<form action="" method="post">
-	<h2><b>Library</b></h2>
-	<input type="submit" name="show" value="All books">
-	<input type="submit" name="show" value="Finished books">
-	<input type="submit" name="show" value="Books in progress">
-	<input type="submit" name="show" value="Planned books">
-	</form>
-	_END;
-	
 	//print_r($_POST);
+	$warning = '';
 	
+	//'add book' form processing
 	if (isset($_POST['add'])){
-		$author = $pdo->quote($_POST['author']);
-		$title = $pdo->quote($_POST['title']);
-		$pages = $pdo->quote($_POST['pages']);
-		$sql = "INSERT INTO books (author, title, pages) VALUES ($author, $title, $pages)";
-		$pdo->query($sql);
+	
+		if ($_POST['author'] != NULL &&
+		    $_POST['title'] != NULL &&
+		    $_POST['pages'] != NULL) {
+		    
+			$author = $pdo->quote($_POST['author']);
+			$title = $pdo->quote($_POST['title']);
+			$pages = $pdo->quote($_POST['pages']);
+			$sql = "INSERT INTO books (author, title, pages) VALUES ($author, $title, $pages)";
+			$pdo->query($sql);
+		}
+		else {
+			$warning = 'Please do not leave empty fields :(';
+		}
 	}
 	
+	//'put a bookmark' form processing
 	elseif (isset($_POST['edit'])){
 		$id = $pdo->quote($_POST['id']);
 		$bookmark = $pdo->quote($_POST['bookmark']);
@@ -55,63 +33,117 @@
 		$pdo->query($sql);
 	}
 	
-	elseif (isset($_POST['show'])){
-		$order = ''; $where = '';
-		
-		switch ($_POST['show']){
-		
-			case 'All books':
-			$order = 'id';
-			break;
-			
-			case 'Finished books':
-			$order = 'id';
-			$where = 'WHERE pages = bookmark';
-			break;
-			
-			case 'Books in progress':
-			$order = 'id';
-			$where = 'WHERE pages <> bookmark AND bookmark <> 0';
-			break;
-			
-			case 'Planned books':
-			$order = 'id';
-			$where = 'WHERE bookmark = 0';
-			break;
-		}
-		
-		$sql = "SELECT * FROM books " . $where . " ORDER BY " . $order;
-		$result = $pdo->query($sql);
-		
-		while ($book = $result->fetch()){
-			$number = $book['id'];
-			$author = $book['author'];
-			$title = $book['title'];
-			$pages = $book['pages'];
-			$bookmark = $book['bookmark'];
-			$last_view = $book['last_view'];
-			
-			echo <<< _END
-			<form action="" method="post">
-			<pre>
-			<big>Number: $number</big>
-			<big>Author: $author</big>
-			<big>Title: $title</big>
-			<big>Pages: $pages</big>
-			<big>Bookmark: $bookmark</big>
-			<big>Last view: $last_view</big>
-			<input type="submit" name="delete" value="Delete">
-			<input type="hidden" name="id" value="$number">
-			
-			</pre>
-			</form>
-			_END;
-		}
-	}
-	
+	//delete button processing
 	elseif (isset($_POST['delete'])){
 		$id = $pdo->quote($_POST['id']);
 		$sql = "DELETE FROM books WHERE id=$id";
 		$pdo->query($sql);
+	}
+	
+	//show 'add book' form
+	echo <<< _END
+	<form action="" method="post">
+	<h2><b>➕ Add book</b></h2>
+	<pre>
+	<big>Author</big> <input type="text" name="author" placeholder="..."><br>
+	<big>Title</big>  <input type="text" name="title" placeholder="..."><br>
+	<big>Pages</big>  <input type="text" name="pages" placeholder="..."><br>
+	<input type="submit" name="add" value="Click to add"> <big><font color="red">$warning</font color="red"></big><br>
+	</pre>
+	<hr>
+	</form>
+	_END;
+	
+	//show 'put a bookmark' form
+	echo <<< _END
+	<form action="" method="post">
+	<h2><b>✍️ Put a bookmark</b></h2>
+	<pre>
+	<big>Number  </big>  <input type="text" name="id" placeholder="..."><br>
+	<big>Bookmark</big>  <input type="text" name="bookmark" placeholder="..."><br>
+	<big>Last view</big> <input type="text" name="last_view" placeholder="dd.mm.yyyy"><br>
+	<input type="submit" name="edit" value="Click to edit"><br>
+	</pre>
+	<hr>
+	</form>
+	_END;
+	
+	//variables for searching books
+	$order = 'id'; $where = ''; $like = '';
+	
+	//search button processing
+	//and also radio buttons
+	if (isset($_POST['find'])){
+		if (isset($_POST['show'])){
+			switch ($_POST['show']){
+					
+				case 'Finished books':
+				$where = 'WHERE pages = bookmark';
+				break;
+					
+				case 'Books in progress':
+				$where = 'WHERE pages <> bookmark AND bookmark <> 0';
+				break;
+					
+				case 'Planned books':
+				$where = 'WHERE bookmark = 0';
+				break;
+			}
+		}
+		//search sql commad depends on 'WHERE' in radio buttons
+		if ($_POST['find'] != NULL){
+			$find = $_POST['find'];
+			if ($where == '') {$like = "WHERE title LIKE '%$find%' OR author LIKE '%$find%'";}
+			else {$like = " AND (title LIKE '%$find%' OR author LIKE '%$find%')";}
+		}
+	}
+
+	$sql = "SELECT * FROM books " . $where . $like . " ORDER BY " . $order;
+	//echo $sql;
+	$result = $pdo->query($sql);
+	
+	//'library' form processing
+	echo <<< _END
+	<form action="" method="post">
+	<h2><b>📚 Library</b></h2>
+	<input type="search" name="find" placeholder="🔍 Write title or author" width="200p">
+	<input type="submit" value="Search"><br><br>
+	<b><big>Filtered by: </big></b>
+	<input type="radio" name="show" value="All books">
+	<label>All books</label>
+	<input type="radio" name="show" value="Finished books">
+	<label>Finished books</label>
+	<input type="radio" name="show" value="Books in progress">
+	<label>Books in progress</label>
+	<input type="radio" name="show" value="Planned books">
+	<label>Planned books</label>
+	</form>
+	_END;
+	
+	//show found records
+	while ($book = $result->fetch()){
+		$number = $book['id'];
+		$author = $book['author'];
+		$title = $book['title'];
+		$pages = $book['pages'];
+		$bookmark = $book['bookmark'];
+		$last_view = $book['last_view'];
+		
+		//form for record 
+		echo <<< _END
+		<form action="" method="post">
+		<pre>
+		<big>Number: $number</big>
+		<big>Author: $author</big>
+		<big>Title: $title</big>
+		<big>Pages: $pages</big>
+		<big>Bookmark: $bookmark</big>
+		<big>Last view: $last_view</big>
+		<input type="submit" name="delete" value="Delete">
+		<input type="hidden" name="id" value="$number">
+			
+		</pre>
+		</form>
+		_END;
 	}
 ?>
